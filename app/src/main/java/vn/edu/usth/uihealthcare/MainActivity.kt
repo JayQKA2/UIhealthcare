8f63f2d6b495424468b1b40601603f6dd71daf06
minhnh296 <hacminh2004@gmail.com>
2024-12-23 18:33:36 +0700

    New year celebration

--- app/src/main/java/vn/edu/usth/uihealthcare/MainActivity.kt
+++ app/src/main/java/vn/edu/usth/uihealthcare/MainActivity.kt
@@ -1,17 +1,25 @@
 package vn.edu.usth.uihealthcare
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.health.connect.client.HealthConnectClient
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import vn.edu.usth.uihealthcare.R
+import vn.edu.usth.uihealthcare.utils.HealthConnectManager
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -30,7 +38,8 @@ class MainActivity : AppCompatActivity() {
                 destination.id == R.id.navigation_sleep ||
                 destination.id == R.id.navigation_step ||
                 destination.id == R.id.navigation_heart ||
-                destination.id == R.id.navigation_measurement
+                destination.id == R.id.navigation_measurement ||
+                destination.id == R.id.navigation_sleep2
             ) {
                 bottomNavigationBar.visibility = View.GONE
                 supportActionBar?.setDisplayHomeAsUpEnabled(true)
